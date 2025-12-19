75c05db897bacc4c50d2defb6ffbb7f6c487982d
radcli14 <radcli14@gmail.com>
2025-12-19 09:18:54 -0500

    template package

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DAE-to-RealityKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DAE-to-RealityKit",
+            targets: ["DAE-to-RealityKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DAE-to-RealityKit"
+        ),
+        .testTarget(
+            name: "DAE-to-RealityKitTests",
+            dependencies: ["DAE-to-RealityKit"]
+        ),
+    ]
+)

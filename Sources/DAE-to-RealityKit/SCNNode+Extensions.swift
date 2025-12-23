eccc21707a19989b88cfb54fe4feb0208dbb5a69
radcli14 <radcli14@gmail.com>
2025-12-23 11:35:31 -0500

    added methods to make sure mesh resources can contain multiple geometries

--- Sources/DAE-to-RealityKit/SCNNode+Extensions.swift
+++ Sources/DAE-to-RealityKit/SCNNode+Extensions.swift
@@ -6,18 +6,46 @@
 //
 
 import Foundation
+import RealityKit
 import SceneKit
 
+@MainActor
 public extension SCNNode {
     /// Recursively find all geometry nodes in this node's hierarchy
     var geometryNodes: [SCNNode] {
         var result: [SCNNode] = []
+        
+        // Add self if it has geometry
         if geometry != nil {
             result.append(self)
         }
-        for child in childNodes {
+        
+        // Create a copy of the childNodes array to avoid issues with concurrent modification
+        let children = Array(childNodes)
+        for child in children {
             result.append(contentsOf: child.geometryNodes)
         }
+        
         return result
+    }
+    
+    // - MARK: RealityKit
+    
+    var descriptors: [MeshDescriptor] {
+        geometryNodes.flatMap { $0.descriptors }
+    }
+    
+    var rkMaterials: [PhysicallyBasedMaterial] {
+        geometryNodes.flatMap { $0.rkMaterials }
+    }
+    
+    func getMeshResource() async -> MeshResource? {
+        do {
+            let sendableDescriptors = UnsafeSendableDescriptors(descriptors: descriptors)
+            return try await MeshResource(from: sendableDescriptors.descriptors)
+        } catch {
+            print("SCNGeometry.getMeshResource() failed because \(error.localizedDescription)")
+            return nil
+        }
     }
 }
